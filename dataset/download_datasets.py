8615b73810220a98f5fd39930e05ad169f826ebb
Daniele Grandi <daniele.grandi@autodesk.com>
2023-03-05 17:29:52 -0800

    updated script to work with other datasets

--- dataset/download_datasets.py
+++ dataset/download_datasets.py
@@ -2,18 +2,22 @@ import urllib.request
 import zipfile
 import os.path
 
-datasets = ['lm']
+datasets = ['lm', 'tless', 'ycbv', 'tyol']
 SRC = "https://bop.felk.cvut.cz/media/data/bop_datasets"
 DST = r"D:\bop_toolkit\data"
 
 for dataset in datasets:
-    for subset in ["base", "models", "test_all", "train_pbr"]:
+    if dataset in ['lm', 'ycbv']:
+        subsets = ["base", "models"] #, "test_all", "train_pbr"]
+    if dataset == 'tless':
+        subsets = ["base", "models"] #, "test_primesense_all", "train_pbr"]
+    for subset in subsets:
         dest_zip = f"{DST}/{dataset}_{subset}.zip"
         if not os.path.isfile(dest_zip):
             print(f"Downloading {dataset}, {subset}...")
             urllib.request.urlretrieve(SRC + f'/{dataset}_{subset}.zip', dest_zip)
         if subset != "base":
-            dest_unzip = DST + "/lm/"
+            dest_unzip = DST + f"/{dataset}/"
         else:
             dest_unzip = DST
         print(f"Unzipping {dataset}, {subset}...")
